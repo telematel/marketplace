b2feb037d31b896128dea114b8610b0e0173cd05
Julio c Bravo <jbr@wedoo.tech>
2021-03-24 13:26:41 -0600

    modif imagne

--- w_split_lead/__manifest__.py
+++ w_split_lead/__manifest__.py
@@ -53,8 +53,8 @@ equal to the percentage of the opportunity.
         'views/inherit_crm_lead_view.xml'
     ],
     'images': [
-        'static/description/Split_description.jpg',
-        'static/description/Split_screenshot.png',
+        'static/description/split_description.jpg',
+        'static/description/split_screenshot.jpg',
     ],
     'demo': [],
     'qweb': [],
