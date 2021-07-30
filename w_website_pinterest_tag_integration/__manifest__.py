8102814ad5ddf58df895bac244bfe3889d3ea6c6
Usdoo Wedoo <jbr@wedoo.tech>
2021-07-30 14:12:59 -0500

    ADD pinterest tag

--- w_website_pinterest_tag_integration/__manifest__.py
+++ w_website_pinterest_tag_integration/__manifest__.py
@@ -1,16 +1,30 @@
 {
     "name": "WEDOO | Pinterest Tag Integration",
-    "version": "13.0.1.0.0",
     'author': 'WEDOO ©',
-    'sequence': 50,
-    "application": False,
-    "installable": True,
     "category": "Website",
-    'website': 'https://www.wedoo.tech',
+    'sequence': 50,
     "summary": "Add support for Pinterest Tag in websites",
+    'website': 'https://www.wedoo.tech',
+    "version": "13.0.1.0.0",
+    'license': 'AGPL-3',
+    'description': """
+        Pinterest Tag Integration
+        =========================
+        Add support for Pinterest Tag in websites.
+    """,
     "depends": ["website"],
     "data": [
         "views/res_config_settings_view.xml",
         "views/website_templates.xml"
     ],
+    'demo': [],
+    'images': [
+        'static/description/img_description.png',
+        'static/description/img_screenshot.png',
+    ],
+    "application": False,
+    "installable": True, 
+    
+    
+    
 }
